2e5093e57fa1c5b9702c41ac2489c0b68bcac816
kapil-velankani <kurmam.kumar@velankani.com>
2016-08-29 12:36:05 +0530

    aws dynodb integration

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,24 @@
+class HomeController < ApplicationController
+  def index
+  	dynamo_db = Aws::DynamoDB::Client.new
+		dynamo_db.create_table(
+      :table_name => "exp_managements_1",
+    	:attribute_definitions => [
+      	{
+        	:attribute_name => :Id,
+        	:attribute_type => :N
+      	}
+    	],
+    	:key_schema => [
+      	{
+        	:attribute_name => :Id,
+        	:key_type => :HASH
+      	}
+    	],
+    	:provisioned_throughput => {
+      	:read_capacity_units => 1,
+      	:write_capacity_units => 1,
+    	}
+  	)
+  end
+end
---
+++ config/initializers/aws.rb
@@ -0,0 +1,5 @@
+Aws.config.update({
+  access_key_id: 'AWS_KEY',
+  secret_access_key: 'AWS_SECRET',
+  region: 'us-east-1'
+})
